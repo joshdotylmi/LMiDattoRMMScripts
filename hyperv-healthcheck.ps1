5ebc938fbd8cfaf9f629b13303773a89bc49912b
joshdotylmi <josh.doty@lmi.net>
2022-09-13 14:31:01 -0700

    push hyperv healthcheck
    a

---
+++ hyperv-healthcheck.ps1
@@ -0,0 +1,33 @@
+function write-DRMMDiag ($messages) {
+    write-host  '<-Start Diagnostic->'
+    foreach ($Message in $Messages) { $Message }
+    write-host '<-End Diagnostic->'
+} 
+function write-DRRMAlert ($message) {
+    write-host '<-Start Result->'
+    write-host "Alert=$message"
+    write-host '<-End Result->'
+}
+
+
+$buildvhdxstoragevolumes = $((Get-VM|Get-VMHardDiskDrive | ForEach-Object {($_.Path).substring(0,1)}|Sort-Object -Unique)) 
+$CurrentVMStorageState = (Get-VM|Get-VMHardDiskDrive | ForEach-Object {Get-Vhd -Verbose $_.Path }| select path,@{label=’Letter’;expression={($_.Path|ft -HideTableHeaders|out-string).substring(0,1)}},@{label=’Size(GB)’;expression={$_.size/1GB –as [int]}})
+
+foreach ($volume in $buildvhdxstoragevolumes) { 
+$sum = (($CurrentVMStorageState |Where-Object {$_.Letter -like "$volume"})."Size(GB)"| Measure-Object -Sum).sum
+$physicalvolumesize=(Get-Volume -DriveLetter $volume).Size/1GB
+
+write-host Currently $volume drive has  $([Math]::Truncate($sum/$physicalvolumesize*100))% provisioned 
+
+if ($sum -gt $physicalvolumesize) {
+    $createdattoalert += "Currently $volume drive has  $([Math]::Truncate($sum/$physicalvolumesize*100))% provisioned"
+write-host hey $volume is overprovisioned btw
+
+}
+}
+if ($createdattoalert -ne $null){
+    write-host $createdattoalert
+    write-DRRMAlert HyperV Host $env:computername is currently over provisioned
+    write-DRMMDiag $createdattoalert
+
+}
